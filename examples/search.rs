bbe00b78244328c45f8bcf7071026410f3b04bb9
igorkrosh <igor.krosh@gmail.com>
2026-03-07 13:08:32 +0300

    Add examples of use smallest and smallest_index functions

--- examples/search.rs
+++ examples/search.rs
@@ -7,7 +7,15 @@ fn main() {
     println!("Array of numbers: {:?}", arr_numbers);
     println!("Array of strings: {:?}", arr_strings);
 
-    println!("Binary Search:");
+    println!("\nBinary Search:");
     println!("Index of 35 in numbers: {:?}", search::binary(&arr_numbers, &35));
     println!("Index of 'cherry' in strings: {:?}", search::binary(&arr_strings, &"cherry"));
+
+    println!("\nSmallest Element:");
+    println!("Smallest number: {:?}", search::smallest(&arr_numbers));
+    println!("Smallest string: {:?}", search::smallest(&arr_strings));
+
+    println!("\nIndex of Smallest Element:");
+    println!("Index of smallest number: {:?}", search::smallest_index(&arr_numbers));
+    println!("Index of smallest string: {:?}", search::smallest_index(&arr_strings));
 }
